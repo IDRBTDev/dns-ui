c938943dabb1d881e6ffd89c31da414f267a12d3
Prabhakaran A <prabhakaran.a@ikcontech.com>
2025-01-08 11:33:39 +0530

    latest

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,12 +1,5 @@
 export const environment = {
     production: true,
-<<<<<<< HEAD
-    apiURL: 'http://10.10.1.138:9002'
-=======
-
-    apiURL: 'http://172.27.10.232:9002',
+    apiURL: 'http://10.10.1.138:9002',
     maxFileSizeMB: 2
-
-
->>>>>>> 55f2d9ceae385e731d4669b1a6e8decd277f4c17
 };
